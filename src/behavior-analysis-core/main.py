4f78c8f8534d16bece2c4e30e5bbe10b234bb5b8
Valentin C <valentin.craciun99@gmail.com>
2020-12-24 15:46:44 +0200

    grpc example

--- src/behavior-analysis-core/main.py
+++ src/behavior-analysis-core/main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+from concurrent import futures
+import logging
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import grpc
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import main_pb2
+import main_pb2_grpc
 
 
-# Press the green button in the gutter to run the script.
+class TestClass(main_pb2_grpc.TestService):
+
+    def testMethod(self, request, context):
+        print(request.text + " " + str(request.number))
+        return main_pb2.TestResponse(number=1, text="test service 1")
+
+    def testMethod2(self, request, context):
+        print(request.text + " " + str(request.number))
+        return main_pb2.TestResponse(number=2, text="test service 1")
+
+
+class TestClass2(main_pb2_grpc.TestService2):
+
+    def testMethod(self, request, context):
+        print(request.text + " " + str(request.number))
+        return main_pb2.TestResponse(number=1, text="test service 2")
+
+    def testMethod2(self, request, context):
+        print(request.text + " " + str(request.number))
+        return main_pb2.TestResponse(number=2, text="test service 2")
+
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    main_pb2_grpc.add_TestServiceServicer_to_server(TestClass(), server)
+    main_pb2_grpc.add_TestService2Servicer_to_server(TestClass2(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    logging.basicConfig()
+    serve()
